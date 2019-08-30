fca0e44be53c234de01e379ebe6c814c867ce0f3
Robert Pedersen <rob84044@gmail.com>
2019-08-29 22:05:40 -0600

    Modified the config file to allow for db type versioning

--- data/config/index.js
+++ data/config/index.js
@@ -1,4 +1,5 @@
 const knex = require('knex')
 const config = require('../../knexfile')
+const dbEnv = process.env.DB_ENV || 'development';
 
-module.exports = knex(config[process.env.DB_ENV])
+module.exports = knex(config[dbEnv])
